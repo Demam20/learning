539fcd48014b08bb45f2afe129284b7caa4fa179
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-04-04 15:30:59 -0700

    toutrial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
+
+
+
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch =useDispatch();
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>Counter{counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged ? <h3>valuble infor </h3> :''}
     </div>
   );
 }
